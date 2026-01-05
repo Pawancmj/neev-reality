f41944f54996d20f0cedea995b78993b409b5f91
CHAMELI <sam.ram.rom.07@gmail.com>
2026-01-05 07:37:16 +0530

    ENQUIRE NOW

--- src/app/reusable_components/stickyicons/stickyicon.tsx
+++ src/app/reusable_components/stickyicons/stickyicon.tsx
@@ -21,7 +21,7 @@ export default function StickyIcons() {
 
           {/* Enquire */}
           <Link
-            href="/contact"
+            href="/Contact"
             className="flex items-center gap-2 bg-[#DBA40D] text-white px-4 py-2 rounded-xl font-medium cursor-pointer"
           >
             <FiMail size={18} />
